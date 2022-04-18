a4d91a3d2f0c9926d07b939f4b06177bdf7bc995
hasanTheBest <rainbowmahmud@gmail.com>
2022-04-18 17:51:20 +0600

    dsiplay user name at checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Register/Loading";
 
 const Checkout = () => {
+  const [user, loading] = useAuthState(auth);
+
+  let content = "";
+  if (loading) {
+    content = <Loading />;
+  }
+
+  if (user) {
+    content = user.displayName ? user.displayName : user.email.split("@")[0];
+  }
+
   return (
     <section className="bg-emerald-50 max-w-6xl mx-auto py-3 px-6 min-h-[calc(100vh-96px)] flex justify-center items-center">
-      <h2 className="text-5xl text-center font-semibold text-emerald-700">
-        checkout page is building
-      </h2>
+      <div>
+        <h2 className="text-5xl text-center font-semibold text-gray-600 mb-6">
+          Welcome, <span className="text-emerald-700">{content}</span>
+        </h2>
+        <h6 className="text-gray-400 text-3xl">
+          Checkout page is under construction. Thank you
+        </h6>
+      </div>
     </section>
   );
 };
